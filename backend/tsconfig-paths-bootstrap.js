eef1334727ee08ef62b11d9f118310fb00a626d5
Farrukh Oripov <farrukh.oripov@gmail.com>
2025-10-18 22:04:48 +0300

    Обновление backend конфигурации
    - Обновлен package.json с зависимостями backend
    - Улучшен tsconfig-paths-bootstrap.js для корректной загрузки модулей

--- backend/tsconfig-paths-bootstrap.js
+++ backend/tsconfig-paths-bootstrap.js
@@ -1,8 +1,8 @@
-const tsConfig = require('./tsconfig.json')
-const tsConfigPaths = require('tsconfig-paths')
+const tsConfig = require("./tsconfig.json");
+const tsConfigPaths = require("tsconfig-paths");
 
-const baseUrl = './src'
+const baseUrl = "./dist";
 tsConfigPaths.register({
   baseUrl,
   paths: tsConfig.compilerOptions.paths,
-})
+});
